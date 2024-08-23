3a8544aa23d58b0f7ee08f03b432da8ca0fab091
pal-oe <fenanyosef@gmail.com>
2024-08-23 19:11:18 +0300

    2024

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,23 +1,21 @@
-# This Puppet manifest optimizes the Nginx configuration to handle high traffic by adjusting worker processes and connection limits
+# This Puppet manifest optimizes Nginx to handle high traffic by adjusting worker processes and connection limits.
 
-# Ensuring the Nginx configuration file contains the necessary optimizations
 file { '/etc/nginx/nginx.conf':
   ensure  => file,
   content => template('nginx/nginx.conf.erb'),
   notify  => Service['nginx'],
 }
 
-# Applying Nginx service restart after configuration change
 service { 'nginx':
   ensure     => running,
   enable     => true,
   subscribe  => File['/etc/nginx/nginx.conf'],
 }
 
-# Increase worker processes and connections to improve handling of simultaneous requests
 exec { 'optimize_nginx':
   command => '/usr/sbin/nginx -s reload',
   onlyif  => 'grep -q "worker_processes auto;" /etc/nginx/nginx.conf',
   notify  => Service['nginx'],
 }
 
+# Ensure the file ends with a new line
