6e441f26665f52ff6f74ff572c38fd6e6b9c8fd0
pal-oe <fenanyosef@gmail.com>
2024-08-23 19:15:44 +0300

    here

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,21 +1,8 @@
 # This Puppet manifest optimizes Nginx to handle high traffic by adjusting worker processes and connection limits.
 
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => template('nginx/nginx.conf.erb'),
-  notify  => Service['nginx'],
+# Fixes an nginx site that can't handle multiple concurrent requests
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
-
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  subscribe  => File['/etc/nginx/nginx.conf'],
-}
-
-exec { 'optimize_nginx':
-  command => '/usr/sbin/nginx -s reload',
-  onlyif  => 'grep -q "worker_processes auto;" /etc/nginx/nginx.conf',
-  notify  => Service['nginx'],
-}
-
-# Ensure the file ends with a new line
